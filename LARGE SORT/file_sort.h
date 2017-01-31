071da1417dcb0a24d4ded570f8e39e8229bc3425
Nikhil <Nikhil@gmail.com>
2017-01-31 23:56:37 +0530

    1 mb

--- LARGE SORT/file_sort.h
+++ LARGE SORT/file_sort.h
@@ -65,11 +65,11 @@ void sort_individual_file(int fd)
 
 	}
 	sorted_file_no+=1;
-	printf("sorted file %llu\n",sorted_file_no-1);
+//	printf("sorted file %llu\n",sorted_file_no-1);
 	close(sorted_new_file);
 //	realloc(heap,0);
 //	free(heap);
-	printf("returning\n");
+//	printf("returning\n");
 
 }
 
@@ -143,26 +143,31 @@ void merge_files()
 	  heap_node heap[MAX_FD];
 	  int sorted_fds[MAX_FD];
 
-	for(i=0;i<MAX_FD;i++)
+	for(i=0;i<MAX_FD;)
 	{
-		
+//		printf("here\n");
 		entry=readdir(dir);
-		
+		//printf("read dir after\n");
 		if(entry==NULL)
 			break;
 
-		if(!strcmp(entry->d_name, ".") || !strcmp(entry->d_name, ".."))
+		if(strcmp(entry->d_name,"trash")!=0 && strcmp(entry->d_name, ".")!=0 && strcmp(entry->d_name, "..")!=0)
 		{
+		i++;
 		count+=1;
+//		printf("inside if\n");
+//		printf("%s\n",entry->d_name);
 		sprintf(sorted_file_name,"%s%s",path,entry->d_name);
 
-		sorted_fds[count]=open(sorted_file_name,O_RDWR);
+		sorted_fds[i]=open(sorted_file_name,O_RDWR);
+
+//		printf("After sorted fd count\n");
 		
 		sprintf(trash_file_name,"%s%s%s",path,"trash/",entry->d_name);
-
+//		printf("%s %s\n",sorted_file_name,trash_file_name);
 		rename(sorted_file_name,trash_file_name);
-
-		 temp=read_heap_node(sorted_fds[count]);
+//		printf("After rename\n");
+		 temp=read_heap_node(sorted_fds[i]);
 
 		if(temp!=NULL)
 		heap_insert(heap,temp);
@@ -172,21 +177,30 @@ void merge_files()
 
 	}
 
-
+//	printf("After loop count= %d\n",count);
 	if(entry==NULL)
 	{
+	printf("in null\n");
 		if(count==1){
 			printf("done sorting\n");return;}
+	dir=opendir(path);
 
 
 	}
 
-	sprintf(sorted_file_name,"sorted%llu",level);
-	sorted_new_file=creat(sorted_file_name,O_RDWR);
+	sprintf(sorted_file_name,"%ssorted%llu.csv",path,level);
+//	printf("sorted file name %s\n",sorted_file_name);
+	sorted_new_file=creat(sorted_file_name,0644);
+	if(sorted_new_file==-1)
+	{
+		perror("error creating file in merge files\n");
+		exit(0);
 
+	}
 	while(!heap_empty(heap))
 	{
 		 heap_node t=pop_min(heap);
+//		 printf("heap %s\n",t.str);
 	 if( -1==(write(sorted_new_file,t.str,strlen(t.str))))
 	 {
 		perror("write error level sorted file\n");
@@ -198,29 +212,31 @@ void merge_files()
 	 
 	
 	}
-	
-
+//	printf("out loop and closing loop\n");
+	close(sorted_new_file);
+//	printf("closed\n");
 	for(int j=0;j<i;j++)
 	{
+		//printf("closing file %d i=%d \n",j,i);
 		close(sorted_fds[j]);
 	}
-	sprintf(trash_file_name,"%s%s","trash/");
+	sprintf(trash_file_name,"%s%s",path,"trash/");
 	
 	DIR* trash_dir=opendir(trash_file_name);
 	struct dirent* trash_entry;
 	while( (trash_entry=readdir(trash_dir))!=NULL)
 		{
-			if( !strcmp(trash_entry->d_name,".") || !strcmp(trash_entry->d_name,".."))
+			if( strcmp(trash_entry->d_name,".")!=0 && strcmp(trash_entry->d_name,"..")!=0)
 			{
 
 		strcat(trash_file_name,trash_entry->d_name);	
-		unlink(trash_file_name);
+unlink(trash_file_name);
 			}
 		}
 
 
 	}
-		
+
 }
 
 void sort(int fd)
@@ -296,7 +312,7 @@ void sort(int fd)
 	
 	dir=opendir(path);
 	 while((entry=readdir(dir))!=NULL ){
-	printf("%s\n",entry->d_name);
+//	printf("%s\n",entry->d_name);
 		if( (strncmp(entry->d_name,"temp",4))==0){
 	 	sprintf(file_name,"%s%s",path,entry->d_name);
 	 	new_file=open(file_name,O_RDWR);
@@ -304,14 +320,14 @@ void sort(int fd)
 			{printf("%s\n",file_name);perror("cant open frpm dirent\n");}
 		sort_individual_file(new_file);
 		close(new_file);
-		printf("before unlink\n");
-		/*if(-1== (remove(file_name)))
+//		printf("before unlink\n");
+		if(-1== (remove(file_name)))
 		{
 			printf("%s\n",file_name);
 			perror("error unlinking file");
 			exit(0);
-		}*/
-		printf("After unlink\n");
+		}
+//		printf("After unlink\n");
 		
 	
 		}
